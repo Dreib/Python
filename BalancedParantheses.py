50fb33baef45d52a5446b33843c2fbe17237d42c
Dreib <andreibuzas@gmail.com>
2022-08-16 10:11:38 +0300

    Stack operation

---
+++ BalancedParantheses.py
@@ -0,0 +1,17 @@
+
+def balanced(expression):
+    # your code goes here
+    bracket_stack = []
+    for char in expression:
+        if char == "(":
+            bracket_stack.insert(0, char)
+        elif char == ")":
+            try:
+                bracket_stack.pop(0)
+            except IndexError:
+                return False
+
+    return bracket_stack == []
+
+
+print(balanced(input()))
